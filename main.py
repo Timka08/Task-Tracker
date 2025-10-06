79d8b0d56c8681635410686202662fdb4e47a31b
Timur <timuroleinikov@gmail.com>
2025-10-06 14:04:19 +0300

    version 0.2.0

--- main.py
+++ main.py
@@ -4,11 +4,11 @@ class TaskTracker:
     def __init__(self):
         self.tasks = []
         self.command_map = {
-        'add': self.add
+        'add': self.add,
         #'status': self.status,
         #'delete': self.delete,
         #'update': self.update,
-        #'list': self.list_tasks
+        'list': self.list_tasks
         #'change': self.change_status
         }
         self.valid_statuses = ['assigned', 'in-progress', 'done']
@@ -31,6 +31,14 @@ class TaskTracker:
         self.tasks.append([title, status])
         print(f"Task '{title}' added")
     
+    def list_tasks(self):
+        if not self.tasks:
+            print("First, add your important tasks here :)")
+            return
+        print("Here are your tasks:")
+        for i, task in enumerate(self.tasks, start=1):
+            print(f"{i}. {task[0]} - {task[1]}")
+    
     
 
 
@@ -40,11 +48,15 @@ def main():
     print("App can add tasks with title and status (assigned, in-progress, done).")
     print("To make a query, enter (delete, status, list, exit, add, change).")
     print("query structure: <command> <command_args>")
-    query = input("Enter query ('exit' to quit): ").lower()
+    query = input("\nEnter query ('exit' to quit): \n")
     while query != 'exit':
+        if not query.strip():
+            print("You could have typed something :\\")
+            query = input("\nEnter query ('exit' to quit): \n")
+            continue
         query_parts = query.split()
-        tracker.command(query_parts[0], query_parts[1:])
-        query = input("Enter query ('exit' to quit): ").lower()
+        tracker.command(query_parts[0].lower(), query_parts[1:])
+        query = input("\nEnter query ('exit' to quit): \n")
         
         
 main()
